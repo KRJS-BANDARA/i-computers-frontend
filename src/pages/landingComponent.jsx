4151071a0ef7833a836dc73a6e195ef144e63f59
KRJS-BANDARA <krjs.bandara@gmail.com>
2026-07-17 22:31:50 +0530

    added landing page component✍🏻

---
+++ src/pages/landingComponent.jsx
@@ -0,0 +1,7 @@
+export default function LandingPage() {
+    return (
+        <div className="w-full h-full flex justify-center items-center">
+            <h1 className="text-2xl font-bold">Landing Component</h1>
+        </div>
+    );
+}
